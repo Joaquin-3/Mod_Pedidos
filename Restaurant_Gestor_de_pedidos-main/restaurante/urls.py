956e4a08353a4ffe03f1a2efaeee9f6a0f7e8108
Joaquin-3 <joaquin.molina18@inacapmail.cl>
2025-12-14 15:43:08 -0300

    Add files via upload

---
+++ Restaurant_Gestor_de_pedidos-main/restaurante/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(("ui.urls","ui"), namespace="ui")),
+    path("mock/", include("mock.urls")),
+    path("api/", include("pedidos.urls")),
+
+    path("api/webhooks/", include(("mock.urls","mock"), namespace="mock")),
+]
+
